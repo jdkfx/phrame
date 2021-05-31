ea2cd75f9fc6ebb8e6e35b81a6d6b4f51b2c9e0d
jdkfx <amateurranch@gmail.com>
2021-05-31 09:36:19 +0900

    :recycle: リファクタリング

--- src/html/index.php
+++ src/html/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// phpinfo();
-// exit;
-
 if ($_SERVER['REQUEST_URI'] == '/') {
     include __DIR__ . '/../app/views/index.php';
     exit;
@@ -17,44 +14,33 @@ foreach ($fullpath as $path) {
     }
 }
 
-if (file_exists( __DIR__ . '/../app/controllers/' . $fullpath[1] . '_controller.php')) {
-    include __DIR__ . '/../app/controllers/' . $fullpath[1] . '_controller.php';
+try {
+    if (file_exists( __DIR__ . '/../app/controllers/' . $fullpath[1] . '_controller.php')) {
+        include __DIR__ . '/../app/controllers/' . $fullpath[1] . '_controller.php';
+        $controllerName = "controllers\\" . $fullpath[1] . "_controller";
+        $controller = new $controllerName();
+    } else {
+        throw new \Exception("error!");
+    }
 
-    $conName = "controllers\\" . $fullpath[1] . "_controller";
-    $con = new $conName();
-}
-
-
-if (isset($fullpath[2])){ // 三項演算子とかでもいいけど、わかりやすさのため
-    $conFunc = $fullpath[2];
-} else {
-    $conFunc = null; 
-}
-
-// var_dump($fullpath, $conFunc);
-// exit;
-
-try{
-    if (method_exists($con, $conFunc)) {
+    isset($fullpath[2]) ? $controllerFunc = $fullpath[2] : $controllerFunc = null;
+    
+    if (method_exists($controller, $controllerFunc)) {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $response = $con->post($_POST); // ここ、conFuncつかわないのかな？
+            $response = $controller->post($_POST); // controllerFuncにすべきか
         } else {
-            // var_dump($conFunc);
-            // exit;
-            $response = $con->$conFunc();
+            $response = $controller->$controllerFunc();
         }
-    } else if (!is_null($conFunc)) {
-        if (!file_exists( __DIR__ . "/../app/views/{$contents}.php")){
+    } else if (!is_null($controllerFunc)) {
+        if (!file_exists( __DIR__ . "/../app/views/{$contents}.php")) {
             throw new \Exception("error!"); 
         }
-        // var_dump($conFunc);
-        //     exit;
-        $response = $con->index();
+        $response = $controller->index();
         include __DIR__ . "/../app/views/{$contents}.php";
     } else {
         throw new \Exception("error!"); 
     }
-} catch(\Exception $e){
-    error_log($e->getFile().$e->getLine().$e->getMessage());
+} catch (\Exception $e) {
+    error_log($e->getFile() . $e->getLine() . $e->getMessage());
     include __DIR__ . '/../app/views/error.php';
 }
