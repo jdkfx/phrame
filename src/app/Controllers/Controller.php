32ba7be1d92ca3966b267ba85e74d0a8eeb5f7b1
jdkfx <amateurranch@gmail.com>
2021-09-02 09:20:15 +0900

    :sparkles: CSRFトークンのバリデーション

--- src/app/Controllers/Controller.php
+++ src/app/Controllers/Controller.php
@@ -2,9 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Middlewares\CsrfToken;
+
 class Controller
 {
     public function __construct(){}
+
+    public function tokenValidate($token)
+    {
+        $csrfToken = new CsrfToken();
+
+        try {
+            if ($csrfToken->validate($token) == false) {
+                exit;
+            }
+        } catch (\Exception $e) {
+            error_log($e->getFile() . $e->getLine() . $e->getMessage());
+        }
+    }
 
     public function redirect($path)
     {
