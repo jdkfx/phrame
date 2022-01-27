336dd44f3bd37cfe3c39ba6b303f5ca048cf7efc
jdkfx <amateurranch@gmail.com>
2022-01-27 22:47:21 +0900

    :rotating_light: POSTに対するテストを追加

--- src/app/Tests/getStatusTest.php
+++ src/app/Tests/getStatusTest.php
@@ -32,6 +32,39 @@ class getStatusTest extends TestCase
         $this->assertEquals(200, $response->getStatusCode());
     }
 
+    public function testGetCreate()
+    {
+        $client = new Client();
+
+        $response = $client->get('http://localhost/blog/create');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPostConfirm()
+    {
+        $client = new Client();
+
+        $response = $client->post('http://localhost/blog/create/confirm', [
+            'title' => 'test',
+            'messages' => 'phpunit-test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPostStore()
+    {
+        $client = new Client();
+
+        $response = $client->post('http://localhost/blog/store', [
+            'title' => 'test',
+            'messages' => 'phpunit-test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
     public function testGet404()
     {
         $client = new Client();
